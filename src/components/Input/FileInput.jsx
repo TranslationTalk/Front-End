16e9cba98c6d3c77107f9cd9afd2bf9cad7c88f0
JISU-Y <80020227+JISU-Y@users.noreply.github.com>
2022-01-12 19:21:20 +0900

    :bulb: Comment: FileInput 컴포넌트 주석 추가
    file 전달 문제 관련 주석 추가

--- src/components/Input/FileInput.jsx
+++ src/components/Input/FileInput.jsx
@@ -7,6 +7,7 @@ import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
 const FileInput = ({ id, onChange, value }) => (
+  // 실제로 받은 file을 어떻게 상위 컴포넌트에 전달할지 나중에 사용하면서 수정 필요
   <Input
     type="file"
     accept=".txt"
