6b89d9520693799b78b43601754fb84fdb8f711a
김형래 <khr157929@gmail.com>
2022-01-13 14:21:35 +0900

    ♻️Refactor : indexJS에 Menu 컴포넌트 추가

--- src/components/index.js
+++ src/components/index.js
@@ -5,5 +5,6 @@ import StatusMessage from './Message/StatusMessage'
 import Input from './Input/index'
 import Tag from './Tag/Tag'
 import TranslatorInfo from './profileInfo/TranslatorInfo'
+import Menu from './menu/Menu'
 
-export { Button, Div, Span, StatusMessage, Input, Tag, TranslatorInfo }
+export { Button, Div, Span, StatusMessage, Input, Tag, TranslatorInfo, Menu }
