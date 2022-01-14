e429e43226fc7e435cfad5a4e223bb30eaeb1542
JISU-Y <80020227+JISU-Y@users.noreply.github.com>
2022-01-14 11:54:15 +0900

    :sparkles: Feat: ChatForm 컴포넌트 추가
    onSubmit props 추가

--- src/components/indicator/BottomIndicator.jsx
+++ src/components/indicator/BottomIndicator.jsx
@@ -2,6 +2,7 @@
  * BottomIndicator
  * 견적 디테일 페이지에서 사용되는 하단에 fixed 바
  * offerPrice: 견적 가격 number (여기서 ,포함된 string으로 변경)
+ * 상담하기 누르면 해당 번역가 채팅창으로 들어가야 하는데 주소가 아직 없음
  */
 import React from 'react'
 import propTypes from 'prop-types'
