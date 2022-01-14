196bddf4cadd04af27d98e1516196c86c6418245
김형래 <khr157929@gmail.com>
2022-01-15 00:19:30 +0900

    ♻️ Refactor : axios 수정

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const instance = axios.create({
   // 기본적으로 우리가 바라볼 서버의 주소
-  baseURL: '',
+  baseURL: 'http://52.79.79.67:3000/',
 
   // 헤더에 넣을 정보
   headers: {
@@ -13,12 +13,19 @@ const instance = axios.create({
 
 instance.interceptors.request.use(function (config) {
   config.headers.common['Authorization'] = `Bearer ${sessionStorage.getItem(
-    'accessToken',
+    'token',
   )}`
   return config
 })
 
 export const apis = {
   // 카카오 로그인
-  login: () => instance.get('/auth/login'),
+  login: () => instance.get('api/auth/kakao/client'), // 일반 유저 로그인
+  translatorLogin: () => instance.get('api/auth/kakao/translator'), // 번역가 로그인
+  developerSignup: (id, auth) =>
+    instance.post(`api/auth/dev/signup`, { id, auth }),
+  developerLogin: id => instance.post(`api/auth/dev/login`, { id }),
+
+  // 번역 요청
+  requestList: () => instance.get(`api/request/list`),
 }
