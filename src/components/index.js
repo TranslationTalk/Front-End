39d451ce0576102add22d2fcd4ad998814727f2f
JISU-Y <80020227+JISU-Y@users.noreply.github.com>
2022-01-15 00:07:07 +0900

    :recycle: Refactor: Component Index 수정
    Card, Chat, Detail, Indicator export 추가

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,25 @@
 // Button
 import Button from './button/Button'
 import TopDownButton from './button/TopDownButton'
+// Card
+import EstimateCard from './card/EstimateCard'
+import EstimateCardMin from './card/EstimateCardMin'
+import PriceCard from './card/PriceCard'
+import ReviewCard from './card/ReviewCard'
+import SummaryCard from './card/SummaryCard'
+import VideoCard from './card/VideoCard'
+// Chat
+import ChatBalloon from './chat/ChatBalloon'
+import ChatDate from './chat/ChatDate'
+import ChatForm from './chat/ChatForm'
+import ChatListCard from './chat/ChatListCard'
+// Detail
+import EstimateDetail from './detail/EstimateDetail'
+// Header
+import PageHeader from './header/PageHeader'
+import SubPageHeader from './header/SubPageHeader'
+// Indicator
+import BottomIndicator from './indicator/BottomIndicator'
 // Input
 import CheckBoxInput from './input/CheckBoxInput'
 import FileInput from './input/FileInput'
@@ -8,21 +27,18 @@ import SelecInput from './input/SelectInput'
 import StarsInput from './input/StarsInput'
 import TextAreaInput from './input/TextAreaInput'
 import TextInput from './input/TextInput'
-// StatusMessage
-import StatusMessage from './message/StatusMessage'
-// Tag
-import Tag from './tag/Tag'
-// TrasnlatorInfo
-import TranslatorInfo from './profileInfo/TranslatorInfo'
 // Menu
 import HamburgerMenu from './menu/HamburgerMenu'
 import ToggleMenu from './menu/ToggleMenu'
+// StatusMessage
+import StatusMessage from './message/StatusMessage'
 // Navbar
 import NavigationUser from './navbar/NavigationUser'
 import NavigationTranslator from './navbar/NavigationTranslator'
-// Header
-import PageHeader from './header/PageHeader'
-import SubPageHeader from './header/SubPageHeader'
+// TrasnlatorInfo
+import TranslatorInfo from './profileInfo/TranslatorInfo'
+// Tag
+import Tag from './tag/Tag'
 
 export {
   Button,
@@ -42,4 +58,16 @@ export {
   NavigationTranslator,
   PageHeader,
   SubPageHeader,
+  EstimateCard,
+  EstimateCardMin,
+  PriceCard,
+  ReviewCard,
+  SummaryCard,
+  VideoCard,
+  ChatBalloon,
+  ChatDate,
+  ChatForm,
+  ChatListCard,
+  EstimateDetail,
+  BottomIndicator,
 }
