942fa6de15ec54684d656ca8b7b434b04efe0541
NamgyungKim <namgyung.kim@gmail.com>
2022-01-15 15:20:21 +0900

    :sparkles: Feat: Router 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import styled from 'styled-components'
+import Router from './pages/Router'
 
 function App() {
   return (
-    <div className="App">
-      <StyledDiv>App components 번역톡 프로젝트!!!!</StyledDiv>
+    <div>
+      <Wrap>
+        <Router />
+      </Wrap>
     </div>
   )
 }
 
-const StyledDiv = styled.div`
-  color: red;
-  font-size: 30px;
+const Wrap = styled.div`
+  min-width: 320px;
+  max-width: 768px;
+  margin: auto;
 `
 
 export default App
---
+++ src/pages/Router.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+// common
+import Login from './common/Login'
+import Chat from './common/Chat'
+import ChatList from './common/ChatList'
+// client
+import ClientEstimateList from './client/ClientEstimateList'
+import ClientHome from './client/ClientHome'
+import ClientRequestForm from './client/ClientRequestForm'
+import RequestList from './client/RequestList'
+import TranslatorDetail from './client/TranslatorDetail'
+// translator
+import EstimateForm from './translator/EstimateForm'
+import MyTranslationList from './translator/MyTranslationList'
+import TranslationList from './translator/TranslationList'
+import TranslatorEstimateDetail from './translator/TranslatorEstimateDetail'
+import TranslatorMyPage from './translator/TranslatorMyPage'
+import TranslatorSignupForm from './translator/TranslatorSignupForm'
+//all
+import All from '../All'
+
+const Router = () => {
+  return (
+    <div>
+      <Routes>
+        {/* all */}
+        <Route path="/test" element={<All />} />
+        {/* common */}
+        <Route path="/" element={<Login />} />
+        <Route path="/chat/:roomid" element={<Chat />} />
+        <Route path="/chat/list" element={<ChatList />} />
+        {/* client */}
+        <Route path="/client/main" element={<ClientHome />} />
+        <Route path="/client/request/list" element={<RequestList />} />
+        <Route path="/client/request" element={<ClientRequestForm />} />
+        <Route
+          path="/client/estimate/list/:id"
+          element={<TranslatorDetail />}
+        />
+        <Route path="/client/estimate/list" element={<ClientEstimateList />} />
+        {/* translator */}
+        <Route path="/translator/signup" element={<TranslatorSignupForm />} />
+        <Route path="/translator/list" element={<TranslationList />} />
+        <Route path="/translator/estimate/form" element={<EstimateForm />} />
+        <Route path="/translator/mypage" element={<TranslatorMyPage />} />
+        <Route
+          path="/translator/translation/list"
+          element={<MyTranslationList />}
+        />
+        <Route
+          path="/translator/estimate/:id"
+          element={<TranslatorEstimateDetail />}
+        />
+      </Routes>
+    </div>
+  )
+}
+
+export default Router
