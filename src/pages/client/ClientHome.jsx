634d97cc5e0adcb0cf9a7e1b34514d884ea3d886
NamgyungKim <namgyung.kim@gmail.com>
2022-01-17 15:56:24 +0900

    :sparkles: Feat: ClientHome 페이지 추가
    isText props를 router에서 넘겨주는 것으로 변경

--- src/pages/client/ClientHome.jsx
+++ src/pages/client/ClientHome.jsx
@@ -1,7 +1,24 @@
+// ClientHome
+// text, 영상 번역요청 고르는 페이지
+
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { NavigationUser, PageHeader } from '../../components'
+import Button from '../../components/button/Button'
 
 const ClientHome = () => {
-  return <div>ClientHome</div>
+  return (
+    <>
+      <PageHeader />
+      <Link to={'/client/request/text'}>
+        <Button content="text번역" />
+      </Link>
+      <Link to={'/client/request/youtube'}>
+        <Button content="영상 번역" />
+      </Link>
+      <NavigationUser />
+    </>
+  )
 }
 
 export default ClientHome
