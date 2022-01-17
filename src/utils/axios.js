220d00f4a2e9c6a293174c1c56d08980c729ecc9
김형래 <khr157929@gmail.com>
2022-01-17 23:40:50 +0900

    ♻️Refactor : 번역가 마이페이지 기능 수정

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -28,7 +28,26 @@ export const apis = {
 
   // 번역가 마이페이지
   getTranslatorMypage: () => instance.get('api/translator/mypage'), // 마이페이지 정보 요청
-  postTranslatorMypage: () => instance.post('api/translator/mypage'), // 첫 로그인시 정보 작성
+  postTranslatorMypage: (
+    name,
+    career,
+    profileFile,
+    language,
+    introduce,
+    taxPossible,
+    cashPossible,
+    isBusiness,
+  ) =>
+    instance.post('api/translator/mypage', {
+      name,
+      career,
+      profileFile,
+      language,
+      introduce,
+      taxPossible,
+      cashPossible,
+      isBusiness,
+    }), // 첫 로그인시 정보 작성
   modifyTranslatorMypage: () => instance.post('api/translator/mypage/modify'), // 마이페이지 정보 수정
 
   // 번역 요청
