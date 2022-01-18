c4b3de281f0b8db7862f4ffe496f894bb5ec2e30
김형래 <khr157929@gmail.com>
2022-01-18 21:52:52 +0900

    ✨Feat : 번역가 마이 페이지 뷰 간단하게 추가

--- src/pages/translator/TranslatorMyPage.jsx
+++ src/pages/translator/TranslatorMyPage.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import { PageHeader, TranslatorInfo, ReviewCard } from '../../components/index'
+import { apis } from '../../utils/axios'
 
 const TranslatorMyPage = () => {
-  return <div>TranslatorMyPage</div>
+  React.useEffect(() => {
+    apis.getReview().then(res => console.log(res.data))
+  })
+  return (
+    <>
+      <PageHeader />
+      <TranslatorInfo />
+      <ReviewCard
+        userName="홍길동"
+        score={4.5}
+        comment="잘해주세요"
+        date="2022-01-17"
+      />
+      <ReviewCard
+        userName="김길동"
+        score={5}
+        comment="만족스럽습니다."
+        date="2022-01-18"
+      />
+    </>
+  )
 }
 
 export default TranslatorMyPage
