ef6f6c6cfc831d34091b695f551cc5b63872b4f1
NamgyungKim <namgyung.kim@gmail.com>
2022-01-20 01:07:22 +0900

    :lipstick: Style: GlobalStyles 공통color, font-size, 웹폰트 정리

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -14,9 +14,92 @@ const GlobalStyles = createGlobalStyle`
     ol, ul, li {
         list-style: none;
     }
-    h1, h2, h3, h4, h5, h6 {
-    font-family:'Maven Pro', sans-serif;
-  }
+    body, button{  
+        font-family: 'Noto Sans KR', sans-serif;
+    }
+
+    :root {
+        /* common-color */
+        --main-color: #3D51FF;
+        --white: #3D51FF;
+        --light-gray: #F4F4F4;
+        --gray: #464646;
+        --light-blue: #EDF2FF;
+        --bark-blue: #363842;
+        --orange-red: #FF5F5F;
+        
+        /* common-font-size */
+        --fs-12: 14px;
+        --fs-14: 16px;
+        --fs-16: 18px;
+        --fs-18: 20px;
+        --fs-20: 24px;
+        --fs-24: 26px;
+        --fs-28: 30px;
+        --fs-32: 34px;
+        --fs-36: 38px;
+        @media screen and (max-width: 360px) { 
+            --fs-12: 12px;
+            --fs-14: 14px;
+            --fs-16: 16px;
+            --fs-18: 18px;
+            --fs-20: 20px;
+            --fs-24: 24px;
+            --fs-28: 28px;
+            --fs-32: 32px;
+            --fs-36: 36px;
+        }
+    }
+
+    /* Noto-Sans-KR 웹폰트 */
+    @font-face { 
+        font-family: 'Noto Sans KR';
+        font-style: normal;
+        font-weight: 100;
+        src: url(//fonts.gstatic.com/ea/notosanskr/v2/NotoSansKR-Thin.woff2) format('woff2'),
+            url(//fonts.gstatic.com/ea/notosanskr/v2/NotoSansKR-Thin.woff) format('woff'),
+            url(//fonts.gstatic.com/ea/notosanskr/v2/NotoSansKR-Thin.otf) format('opentype');
+    }
+    @font-face { 
+        font-family: 'Noto Sans KR';
+        font-style: normal;
+        font-weight: 300;
+        src: url(//fonts.gstatic.com/ea/notosanskr/v2/NotoSansKR-Light.woff2) format('woff2'),
+            url(//fonts.gstatic.com/ea/notosanskr/v2/NotoSansKR-Light.woff) format('woff'),
+            url(//fonts.gstatic.com/ea/notosanskr/v2/NotoSansKR-Light.otf) format('opentype');
+    }
+    @font-face { 
+        font-family: 'Noto Sans KR';
+        font-style: normal;
+        font-weight: 400;
+        src: url(//fonts.gstatic.com/ea/notosanskr/v2/NotoSansKR-Regular.woff2) format('woff2'),
+        url(//fonts.gstatic.com/ea/notosanskr/v2/NotoSansKR-Regular.woff) format('woff'),
+        url(//fonts.gstatic.com/ea/notosanskr/v2/NotoSansKR-Regular.otf) format('opentype');
+    }
+    @font-face { 
+        font-family: 'Noto Sans KR';
+        font-style: normal;
+        font-weight: 500;
+        src: url(//fonts.gstatic.com/ea/notosanskr/v2/NotoSansKR-Medium.woff2) format('woff2'),
+        url(//fonts.gstatic.com/ea/notosanskr/v2/NotoSansKR-Medium.woff) format('woff'),
+        url(//fonts.gstatic.com/ea/notosanskr/v2/NotoSansKR-Medium.otf) format('opentype');
+    }
+    @font-face { 
+        font-family: 'Noto Sans KR';
+        font-style: normal;
+        font-weight: 700;
+        src: url(//fonts.gstatic.com/ea/notosanskr/v2/NotoSansKR-Bold.woff2) format('woff2'),
+        url(//fonts.gstatic.com/ea/notosanskr/v2/NotoSansKR-Bold.woff) format('woff'),
+        url(//fonts.gstatic.com/ea/notosanskr/v2/NotoSansKR-Bold.otf) format('opentype');
+    }
+    @font-face { 
+        font-family: 'Noto Sans KR';
+        font-style: normal;
+        font-weight: 900;
+        src: url(//fonts.gstatic.com/ea/notosanskr/v2/NotoSansKR-Black.woff2) format('woff2'),
+        url(//fonts.gstatic.com/ea/notosanskr/v2/NotoSansKR-Black.woff) format('woff'),
+        url(//fonts.gstatic.com/ea/notosanskr/v2/NotoSansKR-Black.otf) format('opentype');
+    };
 `
 
 export default GlobalStyles
