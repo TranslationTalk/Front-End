fcc30cb3e7e62a05cf35ed91fe8cfd0aa35bf47d
NamgyungKim <namgyung.kim@gmail.com>
2022-01-22 23:33:51 +0900

    :bug: Fix: TopDownButton 컴포넌트 오류 수정
    최하단으로 내려가면 버튼이 동작하지 않았던 부분 개선

--- src/components/button/TopDownButton.jsx
+++ src/components/button/TopDownButton.jsx
@@ -2,48 +2,59 @@
 ===TopDownButton component===
 
 */
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import downBtn from '../../assets/images/downBtn.png'
 import topBtn from '../../assets/images/topBtn.png'
 
 const TopDownButton = () => {
-  React.useEffect(() => {
-    window.addEventListener('scroll', getScrollY)
-
-    return () => window.removeEventListener('scroll', getScrollY)
-  }, [])
-
-  const [topLocation, setTopLocation] = React.useState(0)
-  const innerHeight = window.innerHeight
-  const getScrollY = () => {
-    setTopLocation(window.scrollY)
-  }
+  const [showTopBtn, setshowTopBtn] = useState(false)
+  const [showBottomBtn, setBottomBtn] = useState(false)
+  window.addEventListener('scroll', () => {
+    const innerHeight = document.body.scrollHeight - window.innerHeight
+    let currentScrollHeight = document.documentElement.scrollTop
+    if (currentScrollHeight == 0) {
+      if (showBottomBtn !== true) {
+        setshowTopBtn(false)
+        setBottomBtn(true)
+      }
+    } else if (currentScrollHeight > 0 && currentScrollHeight < innerHeight) {
+      if (showBottomBtn == false && showTopBtn == false) {
+        setBottomBtn(true)
+        setshowTopBtn(true)
+      }
+    } else {
+      if (showBottomBtn !== false) {
+        setshowTopBtn(true)
+        setBottomBtn(false)
+      }
+    }
+  })
 
   return (
     <>
       <TopDownButtonWrap>
-        <TopBtnImgArea
-          topLocation={topLocation}
-          innerHeight={innerHeight}
-          scrollHeight={document.body.scrollHeight}
-        >
+        <TopBtnImgArea showTopBtn={showTopBtn}>
           <img
             src={topBtn}
             alt="상단버튼"
-            onClick={() => window.scrollTo(0, 0)}
+            onClick={() =>
+              window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
+            }
           />
         </TopBtnImgArea>
 
-        <DownBtnImgArea
-          topLocation={topLocation}
-          innerHeight={innerHeight}
-          scrollHeight={document.body.scrollHeight}
-        >
+        <DownBtnImgArea showBottomBtn={showBottomBtn}>
           <img
             src={downBtn}
             alt="하단버튼"
-            onClick={() => window.scrollTo(0, document.body.scrollHeight)}
+            onClick={() =>
+              window.scrollTo({
+                top: document.body.scrollHeight,
+                left: 0,
+                behavior: 'smooth',
+              })
+            }
           />
         </DownBtnImgArea>
       </TopDownButtonWrap>
@@ -55,22 +66,24 @@ const TopDownButtonWrap = styled.div`
   position: fixed;
   bottom: 90px;
   right: 20px;
+  transition: all 0.5;
+  z-index: 10;
 `
 
 const TopBtnImgArea = styled.div`
   transition: 0.5s;
   position: relative;
-  ${props => (props.topLocation < 200 ? `opacity:0;` : `opacity:1`)};
-  ${props =>
-    props.topLocation + props.innerHeight > props.scrollHeight - 200
-      ? `transform: translateY(42px)`
-      : ''}
+  visibility: ${props => (props.showTopBtn ? 'visible' : 'hidden')};
+  opacity: ${props => (props.showTopBtn ? 1 : 0)};
+  cursor: pointer;
 `
 
 const DownBtnImgArea = styled.div`
   transition: 0.5s;
-  opacity: ${props =>
-    props.topLocation + props.innerHeight < props.scrollHeight - 200 ? 1 : 0};
+  visibility: ${props => (props.showBottomBtn ? 'visible' : 'hidden')};
+  opacity: ${props => (props.showBottomBtn ? 1 : 0)};
+
+  cursor: pointer;
 `
 
 export default TopDownButton
