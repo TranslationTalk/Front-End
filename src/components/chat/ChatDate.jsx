343cb3d15b4ad605041a6bef800d23047c421746
JISU-Y <80020227+JISU-Y@users.noreply.github.com>
2022-01-31 10:05:15 +0900

    :lipstick: Style: ChatDate 수평선 추가
    ChatDate 컴포넌트 날짜 양쪽 수평선 추가

--- src/components/chat/ChatDate.jsx
+++ src/components/chat/ChatDate.jsx
@@ -14,11 +14,32 @@ const ChatDate = ({ date }) => {
   const chatDate = date.split('-')[2]
   const day = week[new Date(date).getDay()]
 
-  return <Chat>{`${chatYear}. ${chatMonth}. ${chatDate}. ${day}요일`}</Chat>
+  return (
+    <Wrap>
+      <Line />
+      <Chat>{`${chatYear}. ${chatMonth}. ${chatDate}. ${day}요일`}</Chat>
+      <Line />
+    </Wrap>
+  )
 }
 
+const Wrap = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const Line = styled.div`
+  width: 100%;
+  height: 1px;
+  background-color: var(--gray-bc);
+`
+
 const Chat = styled.p`
+  width: fit-content;
+  min-width: 160px;
   text-align: center;
+  color: var(--gray-bc);
 `
 
 ChatDate.propTypes = {
