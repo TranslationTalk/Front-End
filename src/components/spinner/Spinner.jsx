1c41669821e053f34f0ebb2bcef2b4e4550c0403
JISU-Y <80020227+JISU-Y@users.noreply.github.com>
2022-02-03 10:24:50 +0900

    :recycle: Refactor: Spinner width,height 변경
    width height viewport에서 100%로 변경

--- src/components/spinner/Spinner.jsx
+++ src/components/spinner/Spinner.jsx
@@ -21,8 +21,8 @@ const Spinner = ({ loadingTitle }) => {
 }
 
 const Wrap = styled.div`
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  height: 100%;
   display: flex;
   flex-direction: column;
   justify-content: center;
